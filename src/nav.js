91b85f45b33bcb33ad05297f79798064540b3d3d
Trenton Creamer <trentoncreamer@yahoo.com>
2023-05-30 08:45:38 -0700

    Through page 4 on the lab

--- src/nav.js
+++ src/nav.js
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+
 function Nav() {
   return (
     <nav className="nav nav-tabs mb-2">
       <Link className="nav-link" to="/labs/a3">
         A3
+      </Link>
+      <Link className="nav-link" to="/labs/a4">
+        A4
       </Link>
       <Link className="nav-link" to="/hello">
         Hello
@@ -14,4 +18,5 @@ function Nav() {
     </nav>
   );
 }
+
 export default Nav;
