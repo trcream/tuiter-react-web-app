63b5c1330bff8b6bae4efc4ed3412b8d0ec9a534
Trenton Creamer <trentoncreamer@yahoo.com>
2023-05-30 08:16:30 -0700

    started and accidentally pushed some hw4 code that I needed to revert back

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,18 +1,12 @@
-import Nav from "../nav";
+import { Link } from "react-router-dom";
 import Assignment3 from "./a3";
-import Assignment4 from "./a4";
-import { Routes, Route } from "react-router";
-
 function Labs() {
   return (
     <div>
-      <Nav />
-      <Routes>
-        <Route path="a3" element={<Assignment3 />} />
-        <Route path="a4" element={<Assignment4 />} />
-      </Routes>
+      <Link to="/labs/a3">A3</Link> |<Link to="/hello">Hello</Link> |
+      <Link to="/tuiter">Tuiter</Link>
+      <Assignment3 />
     </div>
   );
 }
-
 export default Labs;
