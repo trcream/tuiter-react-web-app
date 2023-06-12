5ce83f4af286c520dd892ebad8ec320c07308210
Trenton Creamer <trentoncreamer@yahoo.com>
2023-06-12 15:54:11 -0700

    changes for api url

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -4,7 +4,7 @@
 import axios from "axios";
 const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
 // const SERVER_API_URL = "http://localhost:4000/api";
-alert(SERVER_API_URL);
+// alert(SERVER_API_URL);
 
 const USERS_URL = `${SERVER_API_URL}/users`;
 
--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 // const TUITS_API = "http://localhost:4000/api/tuits";
 const TUITS_API = "https://tuiter-node-server-app-7ua3.onrender.com/api/tuits";
+alert(TUITS_API);
 
 // const API_BASE = process.env.REACT_APP_API_BASE;
 // alert(API_BASE);
