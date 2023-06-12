648d303fc9e04db8f2329dd4a5d99952004925b6
Trenton Creamer <trentoncreamer@yahoo.com>
2023-06-12 15:50:13 -0700

    Changed routing to be dynamic

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -2,8 +2,10 @@
 // This is where data is getting send to the server
 
 import axios from "axios";
-// const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
-const SERVER_API_URL = "http://localhost:4000/api";
+const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
+// const SERVER_API_URL = "http://localhost:4000/api";
+alert(SERVER_API_URL);
+
 const USERS_URL = `${SERVER_API_URL}/users`;
 
 const api = axios.create({ withCredentials: true });
--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-const TUITS_API = "http://localhost:4000/api/tuits";
+// const TUITS_API = "http://localhost:4000/api/tuits";
+const TUITS_API = "https://tuiter-node-server-app-7ua3.onrender.com/api/tuits";
+
+// const API_BASE = process.env.REACT_APP_API_BASE;
+// alert(API_BASE);
+// const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
   alert(JSON.stringify(tuit));
