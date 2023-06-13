a93d15f3e500f6a9c018ba458368e0aa85e37bfe
Trenton Creamer <trentoncreamer@yahoo.com>
2023-06-13 11:52:46 -0700

    cookie toubleshooting

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,7 +1,5 @@
 import axios from "axios";
-// const TUITS_API = "http://localhost:4000/api/tuits";
-// const TUITS_API = "https://tuiter-node-server-app-7ua3.onrender.com/api/tuits";
-// const TUITS_API =
+// const API_BASE =
 //   "https://tuiter-node-server-app-trcream.herokuapp.com/api/tuits";
 
 const API_BASE = process.env.REACT_APP_API_BASE;
